3bb6dba155b47f64c3fb78122ff696e5c7469635
Rodrigo Moritz <rodrigomoritz.br@gmail.com>
2020-04-15 21:22:40 -0300

    conceitos ReactJs, armazenar repositorios

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories,setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('/repositories').then((response)=>{
+      setRepositories(response.data) 
+    })
+  },[])
+
   async function handleAddRepository() {
-    // TODO
+ 
+    const response = await api.post('repositories',{
+      url: "https://github.com/rodrigomoritz",
+      title: "Meu Github",
+      techs:["Node","reactJS"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories,repository]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    try {
+      await api.delete(`repositories/${id}`);
+    
+      setRepositories(repositories.filter(repository => repository.id !== id)) ; 
+      
+    } catch (error) {
+      alert('Erro ao deletar, tente novamente.')
+    }
+
   }
 
   return (
     <div>
+      <h1>Repositories</h1>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository=><li key={repository.id}>{repository.title} 
+        
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        )}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+      
     </div>
   );
 }
